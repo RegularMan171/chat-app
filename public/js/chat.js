f01eb9e748b4b9af50e303b1b615fea2e468fc6f
Sarath Konuru <sarath.konuru@hitachivantara.com>
2022-04-20 18:52:12 +0530

    broadcast, sharing location, event acknowledgement

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,8 +3,27 @@ const socket = io()
 document.querySelector('#msg-form').addEventListener('submit', (e)=> {
     //to stop default page refresh
     e.preventDefault()
-    const message = e.target.elements.message
-    socket.emit('sendMessage', message)
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage', message, (error) => {
+        if(error) {
+            return console.log(error)
+        }
+        console.log("Message delivered")
+    })
+})
+
+document.querySelector('#loc').addEventListener('click', () => {
+    if(!navigator.geolocation) {
+        return alert('Browser does not support geolocation')
+    }
+    navigator.geolocation.getCurrentPosition((position)=> {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            console.log("location shared")
+        })
+    })
 })
 
 socket.on('message', (msg) => {
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -15,13 +16,29 @@ app.use(express.static(publicDirPath))
 let count = 0
 
 io.on('connection', (socket) => {
+    
     console.log('new connection')
-
-
     socket.emit('message', 'Hello')
+    socket.broadcast.emit('message', 'A new user has joined')
 
-    socket.on('sendMessage', (msg)=> {
+    socket.on('sendMessage', (msg, callback)=> {
+        const filter = new Filter()
+
+        if(filter.isProfane(msg)) {
+            return callback('Profanity not allowed')
+        }
+
         io.emit('message', msg)
+        callback()
+    })
+
+    socket.on('sendLocation', (pos, cb) => {
+        io.emit('message', `https://google.com/maps?q=${pos.latitude},${pos.longitude}`)
+        cb()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user left')
     })
 })
 
