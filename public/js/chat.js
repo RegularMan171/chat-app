9821070a0ab6a4308a71eec810bb52a0503b4879
Sarath Konuru <sarath.konuru@hitachivantara.com>
2022-04-21 11:04:33 +0530

    rooms, add remove users

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,49 @@
 const socket = io()
 
-document.querySelector('#msg-form').addEventListener('submit', (e)=> {
+//Elements
+const $messageForm = document.querySelector('#msg-form')
+const $mfInput = $messageForm.querySelector('input')
+const $mfButton = $messageForm.querySelector('button')
+const $locButton = document.querySelector('#loc')
+const $msgs = document.querySelector('#msgs')
+
+//templates
+const msgTemplate = document.querySelector('#msg-template').innerHTML
+const locTemplate = document.querySelector('#loc-template').innerHTML
+
+//options
+const {username, room} = Qs.parse(location.search, {ignoreQueryPrefix: true})
+
+socket.on('message', (msg) => {
+    console.log(msg)
+    const html = Mustache.render(msgTemplate, {
+        msg: msg.text,
+        createdAt: moment(msg.createdAt).format('k:mm')
+    })
+    $msgs.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMsg', (url) => {
+    const html = Mustache.render(locTemplate, {
+        url,
+        createdAt: moment(url.createdAt).format('k:mm')
+    })
+    $msgs.insertAdjacentHTML('beforeend', html)
+})
+
+$messageForm.addEventListener('submit', (e)=> {
     //to stop default page refresh
     e.preventDefault()
+    //disabling the button
+    $mfButton.setAttribute('disabled', 'disabled')
+
     const message = e.target.elements.message.value
     socket.emit('sendMessage', message, (error) => {
+        //enabling button
+        $mfButton.removeAttribute('disabled', 'disabled')
+        $mfInput.value=''
+        $mfInput.focus()
+
         if(error) {
             return console.log(error)
         }
@@ -12,20 +51,24 @@ document.querySelector('#msg-form').addEventListener('submit', (e)=> {
     })
 })
 
-document.querySelector('#loc').addEventListener('click', () => {
+$locButton.addEventListener('click', () => {
     if(!navigator.geolocation) {
         return alert('Browser does not support geolocation')
     }
+    //disabling location button
+    $locButton.setAttribute('disabled', 'disabled')
+
     navigator.geolocation.getCurrentPosition((position)=> {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+            //enabling location button
+            $locButton.removeAttribute('disabled', 'disabled')
             console.log("location shared")
         })
     })
 })
 
-socket.on('message', (msg) => {
-    console.log(msg)
-})
+socket.emit('join', {username,room})
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
+const {generateMsg, generateLoc} = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -18,27 +19,30 @@ let count = 0
 io.on('connection', (socket) => {
     
     console.log('new connection')
-    socket.emit('message', 'Hello')
-    socket.broadcast.emit('message', 'A new user has joined')
+
+    socket.on('join', ({username, room}) => {
+        socket.join(room)
+        console.log(username,room)
+        socket.emit('message', generateMsg('Welcome!'))
+        socket.broadcast.to(room).emit('message', generateMsg(`${username} has joined!`))
+    })
 
     socket.on('sendMessage', (msg, callback)=> {
         const filter = new Filter()
-
         if(filter.isProfane(msg)) {
             return callback('Profanity not allowed')
         }
-
-        io.emit('message', msg)
+        io.to('users').emit('message', generateMsg(msg))
         callback()
     })
 
     socket.on('sendLocation', (pos, cb) => {
-        io.emit('message', `https://google.com/maps?q=${pos.latitude},${pos.longitude}`)
+        io.emit('locationMsg', generateLoc(`https://google.com/maps?q=${pos.latitude},${pos.longitude}`))
         cb()
     })
 
     socket.on('disconnect', () => {
-        io.emit('message', 'A user left')
+        io.emit('message', generateMsg('A user left'))
     })
 })
 
